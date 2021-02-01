3d06e4c79a102eda7dedec17eeee9c93528c631e
Morgan Bradford <morganjbradford95@gmail.com>
2021-02-01 17:47:58 -0600

    add inputs for title and post. Add countWords, countVowels, countConsonants
    Co-authored-by: Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>

--- src/journal.js
+++ src/journal.js
@@ -2,3 +2,15 @@ export function Entry(title, post) {
   this.title = title;
   this.post = post;
 }
+
+Entry.prototype.countWords = function() {
+  return this.post.split(" ").length;
+};
+
+Entry.prototype.countVowels = function() {
+  return this.post.match(/[aeiou]/gi).length;
+};
+
+Entry.prototype.countConsonants = function() {
+  return this.post.match(/[a-z]/gi).length - this.countVowels();
+};
--- src/main.js
+++ src/main.js
@@ -3,3 +3,9 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import { Entry } from './journal.js';
+
+$("form#journal-entry").submit(function(event) {
+  event.preventDefault();
+  new Entry();
+  console.log("Entry submitted successfully.");
+});
