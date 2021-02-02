e20fcb8e9b033c5bdf1a0c106f47863c84be86ea
Morgan Bradford <morganjbradford95@gmail.com>
2021-02-01 19:04:47 -0600

    add Entry.prototype.getTeaser

--- src/journal.js
+++ src/journal.js
@@ -1,3 +1,11 @@
+export function Journal() {
+  this.entries = {};
+}
+Journal.prototype.addEntry = function(entry) {
+  entry.date = Date.now();
+  this.entries[entry.date] = entry;
+};
+
 export function Entry(title, post) {
   this.title = title;
   this.post = post;
@@ -13,4 +21,12 @@ Entry.prototype.countVowels = function() {
 
 Entry.prototype.countConsonants = function() {
   return this.post.match(/[a-z]/gi).length - this.countVowels();
+};
+
+Entry.prototype.getTeaser = function() {
+  if (this.countWords() > 8) {
+    return this.post.split(" ").slice(0, 8).join(" ") + "...";
+  } else {
+    return this.post;
+  }
 };
